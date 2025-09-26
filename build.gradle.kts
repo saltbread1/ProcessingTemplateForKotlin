b5679ab0385f58ec3b205d5ad08bbf946531fa64
saltbread1 <takenokonosato913@icloud.com>
2025-09-27 00:39:34 +0900

    update dependencies for platform-specific JOGL support

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,18 +11,52 @@ version = "1.0"
 
 repositories {
     mavenCentral()
-    maven (url = "https://jogamp.org/deployment/maven/")
-    maven (url = "https://maven.scijava.org/content/repositories/public/")
+    flatDir { dirs("libs") }
 }
 
-dependencies {
-    implementation("org.processing:core:4.3.4")
+val osName = System.getProperty("os.name").lowercase()
+val osArch = System.getProperty("os.arch").lowercase()
 
-    /**** JOGL natives ****/
-    runtimeOnly("org.jogamp.gluegen:gluegen-rt-natives-all:2.5.0")
-    runtimeOnly("org.jogamp.jogl:jogl-all-natives-all:2.5.0")
-    runtimeOnly("org.jogamp.jogl:nativewindow:2.5.0")
-    runtimeOnly("org.jogamp.jogl:nativewindow-main:2.5.0")
+dependencies {
+    // processing core
+    implementation(group = "", name = "core", version = "4.4.7")
+
+    // jogl
+    implementation(group = "", name = "jogl-all", version = "2.5.0")
+    implementation(group = "", name = "gluegen-rt", version = "2.5.0")
+
+    // jogl natives
+    when
+    {
+        osName.contains("mac") ->
+        {
+            runtimeOnly(group = "", name = "jogl-all-2.5.0-natives-macosx-universal", version = "2.5.0")
+            runtimeOnly(group = "", name = "gluegen-rt-2.5.0-natives-macosx-universal", version = "2.5.0")
+        }
+
+        osName.contains("windows") ->
+        {
+            runtimeOnly(group = "", name = "jogl-all-2.5.0-natives-windows-amd64", version = "2.5.0")
+            runtimeOnly(group = "", name = "gluegen-rt-2.5.0-natives-windows-amd64", version = "2.5.0")
+        }
+
+        osName.contains("linux") ->
+        {
+            // Linux
+            val arch = when
+            {
+                osArch.contains("aarch64") || osArch.contains("arm64") -> "aarch64"
+                else -> "amd64"
+            }
+            runtimeOnly(group = "", name = "jogl-all-2.5.0-natives-linux-$arch", version = "2.5.0")
+            runtimeOnly(group = "", name = "gluegen-rt-2.5.0-natives-linux-$arch", version = "2.5.0")
+        }
+
+        else ->
+        {
+            println("Unsupported OS: $osName")
+        }
+    }
 }
 
 kotlin {
@@ -47,8 +81,6 @@ tasks.named<ShadowJar>("shadowJar")
     }
 }
 
-
-/**** custom tasks ****/
 
 // java exec
 tasks.register<JavaExec>("runMain") {
